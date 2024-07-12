2d7584f6af33938e219185bdd3f48e2773937cb1
Aminul Ali <alomamin963@gmail.com>
2024-07-12 19:12:56 +0530

    Responsive Design

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // script for javascript
+const downloadButton = document.querySelector('.downloadResume');
+
+downloadButton.addEventListener('click', function() {
+    const fileUrl = './images/IMG_20240712_113356.jpg';
+    const fileName ='Aminuls-resume';
+
+    const link = document.createElement("a");
+    link.href = fileUrl;
+    link.download = fileName;
+
+    link.click();
+})
